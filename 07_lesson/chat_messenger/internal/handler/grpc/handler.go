670b4726cd674c3c7922b8e690b09115a262d21a
Alice Martynova <alicemartynova94@gmail.com>
2026-09-14 11:56:35 +0300

    add grpc http dummy handlers

---
+++ 07_lesson/chat_messenger/internal/handler/grpc/handler.go
@@ -0,0 +1,65 @@
+package grpc
+
+import (
+	"context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"solvery/07_lesson/chat_messenger/gen/messenger/v1"
+)
+
+type Handler struct {
+	messenger.UnimplementedMessengerServiceServer
+}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (*Handler) CreateSession(ctx context.Context, req *messenger.CreateSessionRequest) (*messenger.Session, error) {
+	return nil, status.Error(codes.Unimplemented, "not implemented")
+}
+
+func (*Handler) DeleteSession(ctx context.Context, req *emptypb.Empty) (*messenger.DeleteSessionResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "not implemented")
+}
+
+func (*Handler) CreateChat(ctx context.Context, req *messenger.CreateChatRequest) (*messenger.Chat, error) {
+	return nil, status.Error(codes.Unimplemented, "not implemented")
+}
+
+func (*Handler) GetChat(ctx context.Context, req *messenger.GetChatRequest) (*messenger.Chat, error) {
+	return nil, status.Error(codes.Unimplemented, "not implemented")
+}
+
+func (*Handler) UpdateChat(ctx context.Context, req *messenger.UpdateChatRequest) (*messenger.Chat, error) {
+	return nil, status.Error(codes.Unimplemented, "not implemented")
+}
+
+func (*Handler) DeleteChat(ctx context.Context, req *messenger.DeleteChatRequest) (*messenger.DeleteChatResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "not implemented")
+}
+
+func (*Handler) AddChatMembers(ctx context.Context, req *messenger.AddChatMembersRequest) (*messenger.AddChatMembersResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "not implemented")
+}
+
+func (*Handler) DeleteChatMember(ctx context.Context, req *messenger.DeleteChatMemberRequest) (*messenger.DeleteChatMemberResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "not implemented")
+}
+
+func (*Handler) CreateMessage(ctx context.Context, req *messenger.CreateMessageRequest) (*messenger.Message, error) {
+	return nil, status.Error(codes.Unimplemented, "not implemented")
+}
+
+func (*Handler) GetMessages(ctx context.Context, req *messenger.GetMessagesRequest) (*messenger.GetMessagesResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "not implemented")
+}
+
+func (*Handler) DeleteMessage(ctx context.Context, req *messenger.DeleteMessageRequest) (*messenger.DeleteMessageResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "not implemented")
+}
+
+func (*Handler) EditMessage(ctx context.Context, req *messenger.EditMessageRequest) (*messenger.Message, error) {
+	return nil, status.Error(codes.Unimplemented, "not implemented")
+}
---
+++ 07_lesson/chat_messenger/internal/handler/http/handler.go
@@ -0,0 +1,60 @@
+package http
+
+import (
+	"net/http"
+	"solvery/07_lesson/chat_messenger/gen/openapi"
+)
+
+type Handler struct{}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) CreateSession(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "not implemented", http.StatusNotImplemented)
+}
+
+func (h *Handler) DeleteSession(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "not implemented", http.StatusNotImplemented)
+}
+
+func (h *Handler) CreateChat(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "not implemented", http.StatusNotImplemented)
+}
+
+func (h *Handler) GetChat(w http.ResponseWriter, r *http.Request, chatID openapi.ChatID) {
+	http.Error(w, "not implemented", http.StatusNotImplemented)
+}
+
+func (h *Handler) UpdateChat(w http.ResponseWriter, r *http.Request, chatID openapi.ChatID) {
+	http.Error(w, "not implemented", http.StatusNotImplemented)
+}
+
+func (h *Handler) DeleteChat(w http.ResponseWriter, r *http.Request, chatID openapi.ChatID) {
+	http.Error(w, "not implemented", http.StatusNotImplemented)
+}
+
+func (h *Handler) AddChatMembers(w http.ResponseWriter, r *http.Request, chatID openapi.ChatID) {
+	http.Error(w, "not implemented", http.StatusNotImplemented)
+}
+
+func (h *Handler) DeleteChatMember(w http.ResponseWriter, r *http.Request, chatID openapi.ChatID) {
+	http.Error(w, "not implemented", http.StatusNotImplemented)
+}
+
+func (h *Handler) CreateMessage(w http.ResponseWriter, r *http.Request, chatID openapi.ChatID) {
+	http.Error(w, "not implemented", http.StatusNotImplemented)
+}
+
+func (h *Handler) GetMessages(w http.ResponseWriter, r *http.Request, chatID openapi.ChatID, params openapi.GetMessagesParams) {
+	http.Error(w, "not implemented", http.StatusNotImplemented)
+}
+
+func (h *Handler) DeleteMessage(w http.ResponseWriter, r *http.Request, chatID openapi.ChatID, messageID openapi.MessageID) {
+	http.Error(w, "not implemented", http.StatusNotImplemented)
+}
+
+func (h *Handler) EditMessage(w http.ResponseWriter, r *http.Request, chatId openapi.ChatID, messageId openapi.MessageID) {
+	http.Error(w, "not implemented", http.StatusNotImplemented)
+}
