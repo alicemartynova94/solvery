1949c93d7b244bc6f7211d42d914d30760e1ed95
Alice Martynova <alicemartynova94@gmail.com>
2026-09-15 17:14:39 +0300

    add graceful shutdown and bd connection

--- 07_lesson/chat_messenger/cmd/messenger/main.go
+++ 07_lesson/chat_messenger/cmd/messenger/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"errors"
 	"fmt"
+	_ "github.com/lib/pq"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
 	"gopkg.in/yaml.v3"
-	"log"
 	"net"
 	"net/http"
 	"os"
-	"solvery/07_lesson/chat_messenger/gen/openapi"
+	"os/signal"
+	grpcsrv "solvery/07_lesson/chat_messenger/internal/handler/grpc"
+	httpsrv "solvery/07_lesson/chat_messenger/internal/handler/http"
+	"solvery/07_lesson/chat_messenger/internal/logger"
+	"solvery/07_lesson/chat_messenger/pkg/messenger/v1"
+	"solvery/07_lesson/chat_messenger/pkg/openapi"
 	"strconv"
+	"syscall"
+	"time"
 )
 
 type Config struct {
@@ -17,23 +30,21 @@ type Config struct {
 	} `yaml:"app"`
 
 	HTTP struct {
-		Enabled bool   `yaml:"enabled"`
-		Host    string `yaml:"host"`
-		Port    int    `yaml:"port"`
+		Host string `yaml:"host"`
+		Port int    `yaml:"port"`
 	} `yaml:"http"`
 
 	Swagger struct {
-		Enabled bool   `yaml:"enabled"`
-		Path    string `yaml:"path"`
+		Path string `yaml:"path"`
 	} `yaml:"swagger"`
 
-	RPC struct {
-		Enabled bool   `yaml:"enabled"`
-		Host    string `yaml:"host"`
-		Port    int    `yaml:"port"`
+	GRPC struct {
+		Host string `yaml:"host"`
+		Port int    `yaml:"port"`
 	} `yaml:"grpc"`
 
 	Database struct {
+		Host           string `yaml:"host"`
 		Port           int    `yaml:"port"`
 		User           string `yaml:"user"`
 		Password       string `yaml:"password"`
@@ -44,32 +55,99 @@ type Config struct {
 }
 
 func main() {
+	cfg := MustLoadConfig()
+
+	log, err := logger.NewLogger()
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		_ = log.Sync()
+	}()
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+	errCh := make(chan error, 2)
+
+	db, err := ConnectDB(ctx, cfg)
+	if err != nil {
+		log.Error("db connection failed", zap.Error(err))
+		return
+	}
+	defer db.Close()
+	httpServer := NewHTTPServer(cfg)
+	grpcServer := NewGRPCServer()
+
+	go func() {
+		err := httpServer.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			errCh <- fmt.Errorf("http server: %w", err)
+		}
+	}()
+
+	go func() {
+		listener, err := net.Listen("tcp", net.JoinHostPort(cfg.GRPC.Host, strconv.Itoa(cfg.GRPC.Port)))
+		if err != nil {
+			errCh <- fmt.Errorf("grpc listen: %w", err)
+			return
+		}
+		defer listener.Close()
+
+		err = grpcServer.Serve(listener)
+		if err != nil {
+			errCh <- fmt.Errorf("grpc server: %w", err)
+		}
+	}()
+
+	select {
+	case err := <-errCh:
+		log.Error("server failed", zap.Error(err))
+	case <-ctx.Done():
+		log.Info("shutdown signal received")
+	}
+
+	gracefulShutdown(log, httpServer, grpcServer)
+}
+
+func LoadConfig() (*Config, error) {
 	env := os.Getenv("APP_ENV")
 	if env == "" {
 		env = "local"
 	}
 
-	cfg, err := LoadConfig(fmt.Sprintf("07_lesson/chat_messenger/configs/%s.yaml", env))
+	path := fmt.Sprintf("configs/%s.yaml", env)
+	f, err := os.Open(path)
 	if err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("open config file: %w", err)
+	}
+	defer f.Close()
+
+	var cfg Config
+	decoder := yaml.NewDecoder(f)
+	if err = decoder.Decode(&cfg); err != nil {
+		return nil, fmt.Errorf("yaml decoder: %w", err)
 	}
 
-	httpServer := NewHTTPServer(cfg)
-	err = httpServer.ListenAndServe()
+	return &cfg, nil
+}
+
+func MustLoadConfig() *Config {
+	cfg, err := LoadConfig()
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
+	return cfg
 }
 
 func NewHTTPServer(cfg *Config) *http.Server {
 	mux := http.NewServeMux()
-
-	apiServer := api.NewServer()
+	apiServer := httpsrv.NewHandler()
 	openapi.HandlerFromMux(apiServer, mux)
 
-	swaggerFS := http.FileServer(http.Dir("07_lesson/chat_messenger/api"))
+	swaggerFS := http.FileServer(http.Dir(cfg.Swagger.Path))
 	mux.Handle("GET /swagger/", http.StripPrefix("/swagger/", swaggerFS))
+	mux.Handle("/metrics", promhttp.Handler())
 
 	return &http.Server{
 		Addr:    net.JoinHostPort(cfg.HTTP.Host, strconv.Itoa(cfg.HTTP.Port)),
@@ -77,18 +155,61 @@ func NewHTTPServer(cfg *Config) *http.Server {
 	}
 }
 
-func LoadConfig(path string) (*Config, error) {
-	f, err := os.Open(path)
+func NewGRPCServer() *grpc.Server {
+	server := grpc.NewServer()
+	messenger.RegisterMessengerServiceServer(server, grpcsrv.NewHandler())
+
+	return server
+}
+
+func gracefulShutdown(log *zap.Logger, httpServer *http.Server, grpcServer *grpc.Server) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err := httpServer.Shutdown(ctx)
 	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	var cfg Config
-	decoder := yaml.NewDecoder(f)
-	if err = decoder.Decode(&cfg); err != nil {
-		return nil, err
+		log.Error("http shutdown failed", zap.Error(err))
 	}
 
-	return &cfg, nil
+	grpcDone := make(chan struct{})
+	go func() {
+		grpcServer.GracefulStop()
+		close(grpcDone)
+	}()
+
+	select {
+	case <-grpcDone:
+		log.Info("gRPC server stopped")
+
+	case <-ctx.Done():
+		log.Warn("gRPC graceful shutdown timeout", zap.Error(err))
+		grpcServer.Stop()
+	}
+}
+
+func ConnectDB(ctx context.Context, cfg *Config) (*sql.DB, error) {
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s connect_timeout=%d",
+		cfg.Database.Host,
+		cfg.Database.Port,
+		cfg.Database.User,
+		cfg.Database.Password,
+		cfg.Database.Name,
+		cfg.Database.SSLMode,
+		cfg.Database.ConnectTimeout,
+	)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("pg connection pull: %w", err)
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, time.Duration(cfg.Database.ConnectTimeout)*time.Second)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
+		_ = db.Close()
+		return nil, fmt.Errorf("db ping: %w", err)
+	}
+
+	return db, nil
 }
