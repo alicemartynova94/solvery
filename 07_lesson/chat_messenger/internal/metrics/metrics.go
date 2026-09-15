26b13b94ac1b1608b431405c507e66862bc773dc
Alice Martynova <alicemartynova94@gmail.com>
2026-09-15 17:12:22 +0300

    add simple logger and metrics

---
+++ 07_lesson/chat_messenger/internal/metrics/metrics.go
@@ -0,0 +1,17 @@
+package metrics
+
+import "github.com/prometheus/client_golang/prometheus"
+
+var (
+	RequestsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of HTTP requests.",
+		},
+		[]string{"method", "path"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(RequestsTotal)
+}
